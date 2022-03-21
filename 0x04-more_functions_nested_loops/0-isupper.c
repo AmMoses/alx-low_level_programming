b25e014b80ff6e31cb748db9e05b67ae24479e99
AmMoses <mosesayinde321@gmail.com>
2022-03-21 08:57:09 -0700

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - a function that checks for uppercase characters
- * @c: a variable that stores letters
- * Return: Returns a value of 1 if the variable c contains an uppercase
- * letter.
+ *_isupper - a function that checks for uppercase characters
+ *@c: a variable that stores letters
+ *Return: Returns a value of 1 if the variable c contains an uppercase
+ *letter.
  */
 
 int _isupper(int c)
