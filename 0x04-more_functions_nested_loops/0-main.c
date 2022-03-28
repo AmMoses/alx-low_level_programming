916208c2b3f36ac4e4f2867629a21ff581504682
AmMoses <mosesayinde321@gmail.com>
2022-03-27 17:12:21 -0700

    mains c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char c;
@@ -15,6 +14,5 @@ int main(void)
 	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
-
 	return (0);
 }
